6fc1b07784b64fd22c775d2d1f6fec8439bb80a9
vmultani <vmultani@blueyonder.co.uk>
2020-01-10 20:11:29 +0000

    server.js

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 const express = require("express");
 
+const PORT = process.env.PORT || 8080;
 const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//  Set Handlebars
+
+const exhbs = require("express-handlebars");
+
+app.engine("handlebars", exhbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import Routes and give the server access to them.
+const routes = require("./controllers/burgers_controller");
+app.use(routes);
+
+//  Start your server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
